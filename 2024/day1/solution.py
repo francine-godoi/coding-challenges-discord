67ccb2c528e618cb556387cb68781f9f50e604a8
Francine <francine.godoi@gmail.com>
2024-12-01 20:30:19 -0300

    added solution 2 day 1

--- 2024/day1/solution.py
+++ 2024/day1/solution.py
@@ -1,21 +1,36 @@
-def solution1():
+def get_info():
     with open("input.txt", "r") as file:
         data = file.readlines()
 
     left = []
     right = []
     for line in data:
-        l, r = line.split("  ")
+        l, r = line.strip().split("   ")
         left.append(l)
         right.append(r)
 
     left.sort()
     right.sort()
 
+    return left, right
+
+def solution1():
+    left, right = get_info()
     distance = 0
 
     for l, r in zip(left, right):
         distance += abs(int(l) - int(r))
 
-    print(distance)
+    print(f"Distance: {distance}")
 
+def solution2():
+    left, right = get_info()
+    similarity = 0
+
+    for l in left:
+        similarity += int(l) * right.count(l)
+
+    print(f"Similarity: {similarity}")
+
+solution1()
+solution2()
