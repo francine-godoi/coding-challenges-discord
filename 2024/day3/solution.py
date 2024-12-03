4e657552b8bfc966e48861b26a95c430ca8d5d3b
Francine <francine.godoi@gmail.com>
2024-12-03 20:50:32 -0300

    added solution 2 day 3

--- 2024/day3/solution.py
+++ 2024/day3/solution.py
@@ -16,12 +16,37 @@ def solution1():
         mul_data.append(mul)
 
     total = 0
-    for num in (mul_data):
+    for num in mul_data:
         for n in num:
-            numbers = re.findall(r'\d+', n) 
-            total += (int(numbers[0]) * int(numbers[1]))
+            num1, num2 = re.findall(r'\d+', n) 
+            total += int(num1)* int(num2)
+        
+    return total
+
+def solution2():
+    txt = get_info()
+    mul_data = []
+
+    for line in txt:
+        mul = re.compile(r"mul[(]\d{1,3},\d{1,3}[)] | do[(][)] | don't[(][)]", flags=re.I | re.X)        
+        mul_data.append(mul.findall(line))
+
+    total = 0
+    enable = True
+    for num in mul_data:
+        for n in num:
+            if n[:n.find("(")] == "do":
+                enable = True
+                continue
+            elif n[:n.find("(")] == "don't":
+                enable = False                
+            
+            if enable:
+                num1, num2 = re.findall(r'\d+', n)
+                total += int(num1)* int(num2)
         
     return total
 
 
 print(f"Total uncorrupted multiplication: {solution1()}")
+print(f'Enabled multiplications: {solution2()}')
