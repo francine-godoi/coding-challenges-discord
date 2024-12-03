a15ddeb35e42485947178a4a22bcd9a79ea369cf
Francine <francine.godoi@gmail.com>
2024-12-03 20:53:52 -0300

    added structure day4

---
+++ 2024/day4/solution.py
@@ -0,0 +1,14 @@
+def get_info():
+    with open("input_test.txt", "r") as file:
+        data = file.readlines()
+    
+    return data
+
+def solution1():
+    pass
+
+def solution2():
+    pass
+
+print(f'Resultado: {solution1()}')
+print(f'Resultado: {solution2()}')
