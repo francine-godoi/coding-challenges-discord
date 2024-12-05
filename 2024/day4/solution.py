4ba0adf2d386a1760bb25f2e831dbd631c4413e8
Francine <francine.godoi@gmail.com>
2024-12-04 21:48:47 -0300

    added solution 1 and 2 day 4

--- 2024/day4/solution.py
+++ 2024/day4/solution.py
@@ -1,8 +1,8 @@
 import re
 
 def get_info():
-    with open("input.txt", "r") as file:
-        data = file.readlines()
+    with open("input_test.txt", "r") as file:
+        data = file.read().splitlines()        
     
     return data
 
@@ -11,7 +11,7 @@ def solution1():
 
     count_h = 0
     for line in array: #find horizontal matches       
-        count_h += find_xmas(line.strip())
+        count_h += find_xmas(line)
 
     count_v = 0
     inverted_array = list(zip(*array)) #swapped row and columns to verify vertical matches
@@ -19,28 +19,32 @@ def solution1():
         count_v += find_xmas("".join(line))
 
     count_d = 0
+
     for l, line in enumerate(array): # find diagonal matches
         line = line.strip()
         index = [x.start() for x in re.finditer("X", line)] #find all instances of X to iniciate matches
+        diagonal = []
 
-        for i in index:
+        for i in index:         
+
             if l > 2 and i > 2:  #diagonal up-left
-                if array[l-1][i-1] == "M" and array[l-2][i-2] == "A" and array[l-3][i-3] == "S":
-                    count_d +=1
+                diagonal.append([array[l-1][i-1], array[l-2][i-2], array[l-3][i-3]])  
 
             if l > 2 and i < len(line) - 3: #diagonal up-right
-                if array[l-1][i+1] == "M" and array[l-2][i+2] == "A" and array[l-3][i+3] == "S":
-                    count_d +=1
+                diagonal.append([array[l-1][i+1], array[l-2][i+2], array[l-3][i+3]])                 
                 
             if l < len(array) - 3 and i > 2: #diagonal down-left
-                if array[l+1][i-1] == "M" and array[l+2][i-2] == "A" and array[l+3][i-3] == "S":
-                    count_d +=1
+                diagonal.append([array[l+1][i-1], array[l+2][i-2], array[l+3][i-3]])                      
 
             if l < len(array) - 3 and i < len(line) - 3: #diagonal down-right
-                if array[l+1][i+1] == "M" and array[l+2][i+2] == "A" and array[l+3][i+3] == "S":
-                    count_d +=1
+                diagonal.append([array[l+1][i+1], array[l+2][i+2], array[l+3][i+3]])  
 
+        for dia in diagonal:
+            is_match = "".join(dia)
+            if is_match == "MAS":
+                count_d +=1                         
 
+    print(f"h {count_h} v {count_v} d {count_d}")
     total = count_h + count_v + count_d
     return total
 
