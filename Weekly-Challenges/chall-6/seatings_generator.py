56da117258b8a3d7d23addac1e0497c0011030e9
Fran <francine.godoi@gmail.com>
2024-12-08 11:20:14 -0300

    added solution challenge 6

--- Weekly-Challenges/chall-6/seatings_generator.py
+++ Weekly-Challenges/chall-6/seatings_generator.py
@@ -6,8 +6,8 @@ def print_matrix(col=5,row=3):
         matrix.append([])
         for c in range(col):
             matrix[r].append(random.choice([1,-1]))
-    boy = (random.randint(0,col-1),random.randint(0,row-1))
-    matrix[boy[1]][boy[0]] = 0
+    my_place = (random.randint(0,col-1),random.randint(0,row-1))
+    matrix[my_place[1]][my_place[0]] = 0
     for _ in matrix: print(_)
     
     return matrix    
--- Weekly-Challenges/chall-6/theater_escape.py
+++ Weekly-Challenges/chall-6/theater_escape.py
@@ -1,7 +1,21 @@
 from seatings_generator import print_matrix
 
-def whichExit():
-    matrix = print_matrix(6, 4)
-
-whichExit()
+def whichExit(matrix):
+    left = right = 0
+    for line in matrix:
+        if 0 in line:
+            me = line.index(0)
+            left = sum(1 for i in line[0:me] if i==1)
+            right = sum(1 for i in line[me:] if i==1)
+            break
     
+    if left == right:
+        print("same")
+    elif left > right:
+        print("right")
+    else:
+        print("left")
+
+matrix = print_matrix(6, 4)
+
+whichExit(matrix)
