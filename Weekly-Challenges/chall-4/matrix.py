e68ea4bb5a66e823231c07bec4a71bc563c30ede
Fran <francine.godoi@gmail.com>
2024-12-08 10:35:27 -0300

    added challenge 4 solution

--- Weekly-Challenges/chall-4/matrix.py
+++ Weekly-Challenges/chall-4/matrix.py
@@ -1,8 +1,19 @@
 from matrix_generator import TwoDList
 
+def matrixSum(matrix: list) -> int:
+    total = 0
+    zero_column = []
+    for li, line in enumerate(matrix):        
+        for ci, col in enumerate(line):
+            if matrix[li][ci] == 0:
+                zero_column.append(ci)
+            
+            if li == 0 or ci not in (zero_column):
+                total += col
+    
+    return(total)
 
-def matrixSum():
-    pass
+matrix = TwoDList()
+for _ in matrix: print(_)
 
-
-matrixSum()
+print(f"Result: {matrixSum(matrix)}")
